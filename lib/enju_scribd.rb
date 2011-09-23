0c361288204b4ab9b8352da701b41e11d380fdf7
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2011-09-23 22:06:56 +0900

    acts_as_methods

--- lib/enju_scribd.rb
+++ lib/enju_scribd.rb
@@ -1,19 +1,2 @@
-module EnjuScribd
-  def self.included(base)
-    base.extend ClassMethods
-  end
-
-  module ClassMethods
-    def enju_scribd
-      include EnjuScribd::InstanceMethods
-    end
-  end
-
-  module InstanceMethods
-    attr_accessor :post_to_scribd
-
-    def scribdable?
-      ScribdFu::ContentTypes.include?(get_content_type) && ipaper_id.blank? && post_to_scribd
-    end
-  end
-end
+require 'enju_scribd/acts_as_enju_scribd'
+require 'scribd_fu'
